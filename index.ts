fc7275de6e3b797d3ab2634c5a8ed12bbf785a09
dvelo <52332868+DeveloLongScript@users.noreply.github.com>
2023-04-14 20:50:47 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,9 +2,6 @@
 import {createTemplate} from "./src/types/createInitObject"
 import { playerEvent } from "./src/types/events"
 import { entityEvent } from "./src/types/events"
-
 import { exportTemplate } from "./src/make/exportTemplate"
 
-enum Events {"playerEvent" = playerEvent, "entityEvent" = entityEvent}
-
-export default {createTemplate, exportTemplate, Events}
+export default {createTemplate, exportTemplate, playerEvent, entityEvent}
