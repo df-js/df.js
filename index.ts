54a72d7f157a7ca9f64938c4191f00fae77c37b1
dvelo <52332868+DeveloLongScript@users.noreply.github.com>
2023-04-14 20:49:00 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 // df.js
 import {createTemplate} from "./src/types/createInitObject"
 import { playerEvent } from "./src/types/events"
+import { entityEvent } from "./src/types/events"
+
 import { exportTemplate } from "./src/make/exportTemplate"
+
+enum Events {"playerEvent" = playerEvent, "entityEvent" = entityEvent}
 
 export default {createTemplate, exportTemplate}
